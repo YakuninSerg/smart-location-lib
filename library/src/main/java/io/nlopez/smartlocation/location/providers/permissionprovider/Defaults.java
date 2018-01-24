1e9adfd83ec79c699525c49bcae08ee43886795b
siakunin <79031587670@mail.ru>
2018-01-24 12:10:04 +0300

    Adding class Defaults for store constants

---
+++ library/src/main/java/io/nlopez/smartlocation/location/providers/permissionprovider/Defaults.java
@@ -0,0 +1,12 @@
+package io.nlopez.smartlocation.location.providers.permissionprovider;
+
+import android.Manifest;
+
+/**
+ * Created by siakunin on 24.01.2018.
+ */
+
+public class Defaults {
+    public static final String[] LOCATION_PERMISSIONS = new String[] { Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION };
+}
